c80d5835b07fc7c41378cc9740e6f971db7505d4
ma.BFBee <bfbee@users.noreply.github.com>
2024-12-26 10:57:15 +0800

    Create diy-part1.5.sh

---
+++ diy-part1.5.sh
@@ -0,0 +1,3 @@
+rm -rf ./feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*}
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-packages/openwrt-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-packages/openwrt-passwall-packages
