fb7b4ddd60bc8e2c29ab016a0f14ec7f852c5cf0
ma.BFBee <bfbee@users.noreply.github.com>
2024-12-29 16:36:35 +0800

    Update diy-part1.5.sh
    [test]:update passwall2 to commit[luci: bump to 24.12.12]

--- diy-part1.5.sh
+++ diy-part1.5.sh
@@ -2,4 +2,4 @@ rm -rf ./feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sin
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-packages/openwrt-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-packages/openwrt-passwall-packages
 cd package/openwrt-packages/openwrt-passwall2
-git checkout 4e3932b6cd0ac75095383434c4139d12c63bdbcb
+git checkout 5e2b4a6a6d06b4c01e681e57b4933052d8c18987
