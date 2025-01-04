6ee9112b008d3e5be171c1268d00b7b695dcaaf2
bfbee <bfbee@outlook.com>
2025-01-04 11:41:31 +0000

    modified:   diy-part1.5.sh

--- diy-part1.5.sh
+++ diy-part1.5.sh
@@ -1,5 +1,8 @@
 rm -rf ./feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*}
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-packages/openwrt-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-packages/openwrt-passwall-packages
-#cd package/openwrt-packages/openwrt-passwall2
-#git checkout 4e3932b6cd0ac75095383434c4139d12c63bdbcb
+cd package/openwrt-packages/openwrt-passwall2
+COMMIT_HASH=$(git log --grep="bump" --pretty=format:'%H' -n 1)
+git checkout "$COMMIT_HASH"
+cd ../openwrt-passwall-packages
+git checkout 825aa3a00ad839f3f2521f7e06750a271dabae17
