5410c1c3d7c4c23eb415a71ff9d95bb7e4e7f611
bfbee <bfbee@outlook.com>
2025-01-06 03:00:57 +0000

    升级到ImmortalWRT 24.10

--- diy-part1.5.sh
+++ diy-part1.5.sh
@@ -4,5 +4,5 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt
 cd package/openwrt-packages/openwrt-passwall2
 COMMIT_HASH=$(git log --grep="bump" --pretty=format:'%H' -n 1)
 git checkout "$COMMIT_HASH"
-cd ../openwrt-passwall-packages
-git checkout 825aa3a00ad839f3f2521f7e06750a271dabae17
+# cd ../openwrt-passwall-packages
+# git checkout 825aa3a00ad839f3f2521f7e06750a271dabae17
